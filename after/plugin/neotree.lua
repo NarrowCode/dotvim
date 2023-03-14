b258a6e61c0d1449230de840aa00fee82960511c
Andreas Steinkellner <asteinkellner1@gmail.com>
2023-03-14 10:11:31 +0100

    chore: add neotree

---
+++ after/plugin/neotree.lua
@@ -0,0 +1,2 @@
+vim.keymap.set("n", "<C-b>", ":Neotree toggle<CR>")
+vim.keymap.set("n", "<C-t>", ":Neotree float git_status<CR>")
--- lua/narrowcode/packer.lua
+++ lua/narrowcode/packer.lua
@@ -87,4 +87,14 @@ return require('packer').startup(function(use)
     requires = { 'kyazdani42/nvim-web-devicons', opt = true }
   }
 
+  use {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v2.x",
+    requires = { 
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    }
+  }
+
 end)
