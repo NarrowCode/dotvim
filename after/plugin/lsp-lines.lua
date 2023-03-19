d737b4ffd686b25ef13b78bf9b373b95aa516dfc
Andreas Steinkellner <asteinkellner1@gmail.com>
2023-03-19 23:37:09 +0100

    chore: add toggle shortcut for lsp-lines

---
+++ after/plugin/lsp-lines.lua
@@ -0,0 +1,1 @@
+vim.keymap.set("n", "<leader>l", require("lsp_lines").toggle, { desc = "Toggle lsp_lines" })
--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -5,7 +5,8 @@ require("null-ls").setup({
   sources = {
     null_ls.builtins.formatting.stylua,
     null_ls.builtins.completion.spell,
-    null_ls.builtins.formatting.prettier  -- Make sure prettier is on the $PATH.
+    null_ls.builtins.formatting.prettier,  -- Make sure prettier is on the $PATH.
+    null_ls.builtins.formatting.rustfmt
   },
   -- debug = true, -- Enable and use :NullLsLog to debug.
   -- you can reuse a shared lspconfig on_attach callback here
